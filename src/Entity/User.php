8129cf9cb42113f49f0d9c9c385cfe6ff285b540
husniddin <muhiddinovhusniddin1992@gmail.com>
2026-03-12 22:44:48 +0500

    add (UserCreateAction)

---
+++ src/Entity/User.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Controller\UserCreateAction;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            uriTemplate: '/users/my',
+            controller: UserCreateAction::class,
+            name: 'createUser',
+        ),
+        new Delete(),
+    ],
+    normalizationContext: ['group' => ['user:read']],
+    denormalizationContext: ['group' => ['user:write']],
+)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['user:read'])]
+    private ?int $id = null {
+        get {
+            return $this->id;
+        }
+    }
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:read', 'user:write'])]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['user:write'])]
+    private ?string $password = null;
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
